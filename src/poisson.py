e59cfb91f73b248c9056bc08d3599e38acaa36b6
philippe.bouchet <philippe.bouchet@epita.fr>
2022-11-21 16:12:37 +0100

    Example file to test Dolfinx

---
+++ src/poisson.py
@@ -0,0 +1,51 @@
+import numpy as np
+from dolfinx import fem, io, mesh, plot
+
+import ufl
+from ufl import ds, dx, grad, inner
+
+from mpi4py import MPI
+from petsc4py.PETSc import ScalarType
+
+msh = mesh.create_rectangle(comm=MPI.COMM_WORLD,
+                            points=((0.0, 0.0), (2.0, 1.0)), n=(32, 16),
+                            cell_type=mesh.CellType.triangle,)
+V = fem.FunctionSpace(msh, ("Lagrange", 1))
+
+facets = mesh.locate_entities_boundary(msh, dim=1,
+                                       marker=lambda x: np.logical_or(np.isclose(x[0], 0.0),
+                                                                      np.isclose(x[0], 2.0)))
+
+dofs = fem.locate_dofs_topological(V=V, entity_dim=1, entities=facets)
+
+bc = fem.dirichletbc(value=ScalarType(0), dofs=dofs, V=V)
+
+u = ufl.TrialFunction(V)
+v = ufl.TestFunction(V)
+x = ufl.SpatialCoordinate(msh)
+f = 10 * ufl.exp(-((x[0] - 0.5) ** 2 + (x[1] - 0.5) ** 2) / 0.02)
+g = ufl.sin(5 * x[0])
+a = inner(grad(u), grad(v)) * dx
+L = inner(f, v) * dx + inner(g, v) * ds
+
+problem = fem.petsc.LinearProblem(a, L, bcs=[bc], petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+uh = problem.solve()
+
+with io.XDMFFile(msh.comm, "out_poisson/poisson.xdmf", "w") as file:
+    file.write_mesh(msh)
+    file.write_function(uh)
+
+try:
+    import pyvista
+    cells, types, x = plot.create_vtk_mesh(V)
+    grid = pyvista.UnstructuredGrid(cells, types, x)
+    grid.point_data["u"] = uh.x.array.real
+    grid.set_active_scalars("u")
+    plotter = pyvista.Plotter()
+    plotter.add_mesh(grid, show_edges=True)
+    warped = grid.warp_by_scalar()
+    plotter.add_mesh(warped)
+    plotter.show()
+except ModuleNotFoundError:
+    print("'pyvista' is required to visualise the solution")
+    print("Install 'pyvista' with pip: 'python3 -m pip install pyvista'")
