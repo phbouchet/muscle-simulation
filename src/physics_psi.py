c9bfde3420887a990ba462d2f34d3ac5a37efa6a
philippe.bouchet <philippe.bouchet@epita.fr>
2023-01-09 01:32:49 +0100

    Working on derivatives for Psi Fiber/Iso

--- src/physics_psi.py
+++ src/physics_psi.py
@@ -6,49 +6,19 @@ as well as their derivatives and hessians.
 import numpy as np
 
 def a(t : int) -> float:
-    """ Linear activation function
-    Args:
-        t (int): Time variable
-    """
     return (1/10) * t
 
-def psi_fiber(F : np.ndarray, u : np.ndarray, t : int) -> np.ndarray:
-    """ Eq.8
-    Args:
-        F (np.ndarray): Deformation gradient: (9m, 1)
-        u (np.ndarray): Vertex-wise directional vector: (9m, 1)
-        t (int): Time variable
-    Returns:
-        np.ndarray: (9m, 9m)
-    """
-    return a(t) * u @ F.T @ F @ u.T
+def psi_fiber(F : np.ndarray, u : np.ndarray, t : int) -> int:
+    return a(t) * (F.T @ F @ u.T @ u)[0,0]
 
 def d_psi_fiber(F : np.ndarray, u : np.ndarray, t : int) -> np.ndarray:
-    """ Derivative of Eq.8
-    Args:
-        F (np.ndarray): Deformation gradient: (9m, 1)
-        u (np.ndarray): Vertex-wise directional vector: (9m, 1)
-        t (int): Time variable
-    Returns:
-        np.ndarray: (9m, 9m)
-    """
-    return psi_fiber(F, u, t)
+    return a(t) * (F @ u.T @ u)
 
 def psi_fiber_hessian(F : np.ndarray,  u : np.ndarray, t : int) -> np.ndarray:
-    """ Hessian of Eq.8
-    Args:
-        F (np.ndarray): Deformation gradient: (9m, 1)
-        u (np.ndarray): Vertex-wise directional vector: (9m, 1)
-        t (int): Time variable
-    Returns:
-        np.ndarray: (9m, 9m)
-    """
     return psi_fiber(F, u, t)
 
 def psi_iso(F : np.ndarray):
-    C = np.pi
-    I = np.trace(F @ F.T)
-    return C * (I - 3)
+    return 0
 
 def d_psi_iso(F : np.ndarray):
     return 0
