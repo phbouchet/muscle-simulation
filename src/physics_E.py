0a6e0b8e1de53aac00edb5287cb05f6b81755332
philippe.bouchet <philippe.bouchet@epita.fr>
2023-01-09 01:31:32 +0100

    Removed comments, updated dimensionality

--- src/physics_E.py
+++ src/physics_E.py
@@ -4,49 +4,18 @@ defined in 'EMU: Efficient Muscle Simulation in Deformation Space' by V. Modi et
 """
 
 import numpy as np
-from physics_psi import *
 from physics_Ec  import *
+from physics_psi import *
 
-def E(F : np.ndarray, G : np.ndarray, u : np.ndarray, t : int, α : int) -> np.ndarray:
-    """ Discretized energy Eq.5
-    Args:
-        F (np.ndarray): Deformation gradient: (9m, 1)
-        G (np.ndarray): Deformation gradients according to q: (9m, 3n)
-        u (np.ndarray): Vertex-wise directional vector: (9m, 1)
-        t (int): Time variable
-        α (int): Hyperparameter
-    Returns:
-        np.ndarray: (9m, 9m)
-    """
-    return psi_iso(F) + psi_fiber(F, u, t) + α * E_c(F, G)
+def E(F : np.ndarray, G : np.ndarray, u : np.ndarray, t : int, α : int) -> int:
+    q = argmin_Ec(F, G)
+    return psi_iso(F) + psi_fiber(F, u, t) + α * Ec(F, G, q)[0,0]
 
 def d_E(F : np.ndarray, G : np.ndarray, u : np.ndarray, t : int, α : int) -> np.ndarray:
-    """ Derivative of discretized energy Eq.9
-    Args:
-        F (np.ndarray): Deformation gradient: (9m, 1)
-        G (np.ndarray): Deformation gradients according to q: (9m, 3n)
-        u (np.ndarray): Vertex-wise directional vector: (9m, 1)
-        t (int): Time variable
-        α (int): Hyperparameter
-    Returns:
-        np.ndarray: (9m, 9m)
-    """
-    return d_psi_iso(F) + d_psi_fiber(F, u, t) + α * d_E_c(F, G)
+    return d_psi_iso(F) + d_psi_fiber(F, u, t) + α * d_Ec(F, G)
 
 def E_hessian(F : np.ndarray, G : np.ndarray, u : np.ndarray, t : int, α : int, dim : tuple) -> np.ndarray:
-    """ Hessian of discretized energy Eq.9
-    Args:
-        F (np.ndarray): Deformation gradient: (9m, 1)
-        G (np.ndarray): Deformation gradients according to q: (9m, 3n)
-        u (np.ndarray): Vertex-wise directional vector: (9m, 1)
-        t (int): Time variable
-        α (int): Hyperparameter
-        dim (tuple): Tuple containing values of n and m
-    Returns:
-        np.ndarray: (9m, 9m)
-    """
     n, m = dim
-    
     k = 3*n
 
     w, v = np.linalg.eig(G.T @ G)
@@ -70,21 +39,10 @@ def E_hessian(F : np.ndarray, G : np.ndarray, u : np.ndarray, t : int, α : int,
     return hessian
 
 def E_hessian_inv(F : np.ndarray, G : np.ndarray, u : np.ndarray, t : int, α : int, dim : tuple) -> np.ndarray:
-    """ Inverse hessian of discretized energy Eq.9
-    Args:
-        F (np.ndarray): Deformation gradient: (9m, 1)
-        G (np.ndarray): Deformation gradients according to q: (9m, 3n)
-        u (np.ndarray): Vertex-wise directional vector: (9m, 1)
-        t (int): Time variable
-        α (int): Hyperparameter
-        dim (tuple): Tuple containing values of n and m
-    Returns:
-        np.ndarray: (9m, 9m)
-    """
     n, m = dim 
     k = 3*n
 
-    w, v = np.linalg.eig(G.T @ G)
+    w, v = np.linalg.eig(np.linalg.inv(G.T @ G))
     eig_val, eig_vec = np.array([w]), np.squeeze(np.array([v]))
 
     if (3*n > 48):
@@ -99,8 +57,8 @@ def E_hessian_inv(F : np.ndarray, G : np.ndarray, u : np.ndarray, t : int, α : 
     H_inv = np.linalg.inv(H) # (9m, 9m)
 
     B = Φ @ G.T # (k, 9m)
-    
+
     # Inverse of hessian expression
-    hessian_inv = H_inv + α * H_inv @ B.T @ Λ - α*B @ H_inv @ B.T @ B @ H_inv
+    hessian_inv = H_inv + α * H_inv @ B.T @ np.linalg.inv(Λ - α*B @ H_inv @ B.T) @ B @ H_inv
 
     return hessian_inv
