26ce092cc63ba5d10d047b14bee60af57c944321
philippe.bouchet <philippe.bouchet@epita.fr>
2023-01-14 21:28:54 +0100

    Added mesh export for tet visualization

--- src/mesh_tools.py
+++ src/mesh_tools.py
@@ -1,6 +1,7 @@
+import shutil
 import numpy as np
 
-def load_obj_mesh(mesh_path):
+def load_mesh(mesh_path : str):
     vertex_data = []
     face_data = []
     for line in open(mesh_path, "r"):
@@ -19,9 +20,19 @@ def load_obj_mesh(mesh_path):
     faces = np.array(face_data)
     return vertices, faces 
 
-def get_triangle(vertices : np.ndarray, faces : np.ndarray) -> np.ndarray:
-    triangle = np.zeros((3, 3))
-    for i in range(3):
-        for j in range(3):
-            triangle[i, j] = vertices[faces[i] - 1, j]
-    return triangle
+def export_mesh(mesh : np.ndarray, base_file : str, out_file : str):
+    shutil.copy(base_file, out_file)
+
+    with open(base_file, 'r') as file:
+        data = file.readlines()
+
+    tet_i = 0
+    l = len(data)
+    for i in range(l):
+        if data[i][0] != 'v' or data[i][1] != ' ':
+            continue
+        data[i] = f"v {mesh[tet_i*3][0]} {mesh[tet_i*3+1][0]} {mesh[tet_i*3+2][0]}\n"
+        tet_i += 1
+
+    with open(out_file, 'w') as out_file:
+        out_file.writelines(data)
