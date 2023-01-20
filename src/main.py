49cef46c7fa14c4538cc388832b1dbe085935d63
philippe.bouchet <philippe.bouchet@epita.fr>
2023-01-20 02:05:32 +0100

    Implemented command-line arguments parser

--- src/main.py
+++ src/main.py
@@ -1,21 +1,36 @@
+#!/usr/bin/env python3
+
 import os
 import numpy as np
+import argparse
 
-from EMU import *
 from mesh_tools import *
+from physics_EMU import *
 
 np.random.seed(42)
 
-mesh_file = "simple_tet.obj"
-mesh_path = f"{os.getcwd()}/data/{mesh_file}"
-vertices, faces = load_mesh(mesh_path)
+def main(mesh_file : str, iterations : int = None):
+    mesh_path = f"{os.getcwd()}/{mesh_file}"
+    vertices, _ = import_mesh(mesh_path)
 
-n = len(vertices)
-m = int(n / 4)
-dim = (n, m)
+    n = len(vertices) 
+    m = int(n/4)
+    dim = (n, m)
 
-F = np.ravel(np.identity(3))[:, None]
-Q = np.ravel(np.array(vertices))[:, None]
-u = np.ones((3*m, 1))
+    F = np.tile(np.ravel(np.identity(3))[:, None], (m, 1))
+    Q = np.ravel(np.array(vertices))[:, None]
+    u = np.ones((3*m, 1))
 
-EMU(F, Q, u, dim, mesh_path)
+    EMU(F, Q, u, dim, mesh_path, iterations)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Input parameters for EMU algorithm.")
+    parser.add_argument('file', type=str, help="Target mesh file in obj format for simulation.")
+    parser.add_argument('-i', '--iterations', type=int, help="Number of iterations")
+    
+    args = parser.parse_args()
+
+    mesh_file = args.file
+    iterations = args.iterations
+
+    main(mesh_file, iterations)
